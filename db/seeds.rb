f7b0ff7c8e8911d9e78c657e27000f1213f86045
Nerian <siotopo@gmail.com>
2012-03-04 19:31:15 +0100

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# encoding: utf-8
+
+Course.create(full_name: 'Metodología y tecnología de la programación I', abbreviation: 'MTP')
+Course.create(full_name: 'Fundamentos matemáticos de la informática I', abbreviation: 'FMI1')
+Course.create(full_name: 'Fundamentos de sistemas de información', abbreviation: 'FSI')
+Course.create(full_name: 'Técnicas de organización y gestión empresarial I', abbreviation: 'TOG1')
+Course.create(full_name: 'Estructura y tecnología de computadores', abbreviation: 'ETC')
+Course.create(full_name: 'Estructura de datos y de la información', abbreviation: 'EDI')
+
+Course.create(full_name: 'Sistemas integrados de gestión I', abbreviation: 'SIG1')
+Course.create(full_name: 'Bases de datos', abbreviation: 'BD')
+Course.create(full_name: 'Estadística', abbreviation: 'EST')
+Course.create(full_name: 'Técnicas de organización y gestión empresarial II', abbreviation: 'TOG2')
+Course.create(full_name: 'Fundamentos matemáticos de la informática II', abbreviation: 'FMI2')
+Course.create(full_name: 'Metodología y tecnología de la programación II', abbreviation: 'MTP2')
+Course.create(full_name: 'Sistemas operativos', abbreviation: 'SO')
+Course.create(full_name: 'Ingeniería del software de gestión I', abbreviation: 'ISG1')
+
+Course.create(full_name: 'Sistemas Hipermedia', abbreviation: 'SH')
+Course.create(full_name: 'Redes Telemáticas', abbreviation: 'RT')
+Course.create(full_name: 'Sistemas integrados de gestión II', abbreviation: 'SIG2')
+Course.create(full_name: 'Seguridad Informática', abbreviation: 'SI')
+Course.create(full_name: 'Ingeniería del software de gestión II', abbreviation: 'ISG2')
+Course.create(full_name: 'Sistemas de información global', abbreviation: 'SIGL')
+Course.create(full_name: 'Seguridad y medios de pagos', abbreviation: 'SYP')
+Course.create(full_name: 'Programación Web Avanzada', abbreviation: 'PWA')
+Course.create(full_name: 'Ampliación de bases de datos', abbreviation: 'BD2')
+
+20.times do |i|
+  Fabricate(:user)
+end
+
+10.times do
+  Fabricate(:user, :first_name => 'hugo')
+end
--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -1,2 +1,6 @@
 Fabricator(:user) do
+  first_name { Forgery(:name).first_name }
+  last_name { Forgery(:name).last_name }
+  dni { Forgery(:basic).password(:at_least => 9, :at_most => 9) }
+  email { Forgery(:internet).email_address }
 end
