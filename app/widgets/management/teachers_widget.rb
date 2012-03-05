072efc1b257210b7d80624532e565764440e65bf
Nerian <siotopo@gmail.com>
2012-03-05 14:21:24 +0100

    search event

--- app/widgets/management/teachers_widget.rb
+++ app/widgets/management/teachers_widget.rb
@@ -3,20 +3,21 @@ class Management::TeachersWidget < Apotomo::Widget
   responds_to_event :done
   responds_to_event :add_teacher
   responds_to_event :remove_teacher
+  responds_to_event :search
 
   def display
     @course = Course.find(params[:id])
     @teachers = @course.teachers
     render
   end
-  
+
   def edit
     @course = Course.find(params[:id])
     @teachers = @course.teachers
     @candidates = candidates
-    update :view => :edit    
+    update :view => :edit
   end
-  
+
   def remove_teacher(evt)
     teacher = User.find(evt[:teacher_id])
     course = Course.find(evt[:id])
@@ -26,6 +27,12 @@ class Management::TeachersWidget < Apotomo::Widget
 
   def edit_mode(evt)
     render :state => :edit
+  end
+
+  def search(evt)
+    users = User.search(evt[:query], :match => :all)
+    candidates = users.collect { |user| {'teacher_id' => user.id, 'value' => "#{user.first_name} #{user.last_name}"} }
+    render text: candidates.to_json
   end
 
   def done(evt)
@@ -40,7 +47,7 @@ class Management::TeachersWidget < Apotomo::Widget
       render :state => :edit
     end
   end
-  
+
   def candidates
     User.all.collect { |user| {'teacher_id' => user.id, 'value' => "#{user.first_name} #{user.last_name}"} }
   end
