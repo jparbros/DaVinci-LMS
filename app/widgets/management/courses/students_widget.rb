500cfed5606a2bd46b6f29f52c01e259b0594541
Nerian <siotopo@gmail.com>
2012-03-05 17:09:55 +0100

    refactor

--- app/widgets/management/courses/students_widget.rb
+++ app/widgets/management/courses/students_widget.rb
@@ -1,5 +1,6 @@
 class Management::Courses::StudentsWidget < Apotomo::Widget
-  responds_to_event :edit_mode
+  responds_to_event :set_add_mode
+  responds_to_event :set_remove_mode
   responds_to_event :done
   responds_to_event :add_student
   responds_to_event :remove_student
@@ -9,22 +10,16 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
     @students = @course.students
     render
   end
-
-  def edit
-    @course = Course.find(params[:id])
-    @students = @course.students
-    @candidates = candidates
-    update :view => :edit
+  
+  def set_remove_mode(evt)
+    render :state => :remove_mode
   end
   
-  def add_student(evt)
-    student = User.find(evt[:student_id])
-    @course = Course.find(evt[:id])
-    @course.students << student
-    if @course.save
-      @message = "#{student.name} has been added as a student"
-      render :state => :edit
-    end
+  def remove_mode
+    @course = Course.find(params[:id])
+    @students = @course.students
+    @candidates = candidates(@students)
+    update :view => :remove
   end
   
   def remove_student(evt)
@@ -32,11 +27,28 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
     course = Course.find(evt[:id])
     course.students.delete(student)
     @message = "#{student.name} is no longer a student in #{course.abbreviation}"
-    render :state => :edit
+    render :state => :remove_mode
   end
   
-  def edit_mode(evt)
-    render :state => :edit
+  def set_add_mode(evt)
+    render :state => :add_mode
+  end
+  
+  def add_mode
+    @course = Course.find(params[:id])
+    @students = @course.students
+    @candidates = candidates
+    update :view => :add
+  end
+  
+  def add_student(evt)
+    student = User.find(evt[:student_id])
+    @course = Course.find(evt[:id])
+    @course.students << student
+    if @course.save
+      @message = "#{student.name} has been added as a student in #{@course.abbreviation}"
+      render :state => :add_mode
+    end
   end
   
   def done(evt)
@@ -45,9 +57,9 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
   
   def candidates(users=[])
     if users.empty?
-      return User.all.collect { |user| {'teacher_id' => user.id, 'value' => user.name} }
+      return User.all.collect { |user| {'student_id' => user.id, 'value' => user.name} }
     else
-      return users.collect { |user| {'teacher_id' => user.id, 'value' => user.name} }
+      return users.collect { |user| {'student_id' => user.id, 'value' => user.name} }
     end
   end
 end
