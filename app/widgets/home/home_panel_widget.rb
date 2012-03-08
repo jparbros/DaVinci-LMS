c617cf01c12cce44454228de1849d5f9a2cb9c4d
Nerian <siotopo@gmail.com>
2012-03-08 22:31:28 +0100

    refactor title

--- app/widgets/home/home_panel_widget.rb
+++ app/widgets/home/home_panel_widget.rb
@@ -1,8 +1,4 @@
 class Home::HomePanelWidget < Apotomo::Widget
-  
-  cache :display do |cell, user|
-    user.id.to_s + user.updated_at.to_s
-  end
 
   def display(user)
     @courses_as_student = user.student_in
