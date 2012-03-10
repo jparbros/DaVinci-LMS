a6e4ffea97654e5185b65f100653c70b8c48667e
Nerian <siotopo@gmail.com>
2012-03-10 13:15:28 +0100

    validate tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,9 @@ class Task
   field :title, type: String
   field :last_day_to_submit, type: Date, default: Date.current
   
+  validates :title, :presence => true
+  validates :last_day_to_submit, :presence => true
+  validates :description, :presence => true  
+  
   belongs_to :course
 end
