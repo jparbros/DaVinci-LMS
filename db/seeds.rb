390f4ae88b2ebfe2085e28b34a5ab141df07d71b
Nerian <siotopo@gmail.com>
2012-03-12 15:27:14 +0100

    change seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,19 +26,29 @@ Course.create(full_name: 'Seguridad y medios de pagos', abbreviation: 'SYP')
 Course.create(full_name: 'Programación Web Avanzada', abbreviation: 'PWA')
 Course.create(full_name: 'Ampliación de bases de datos', abbreviation: 'BD2')
 
-admin = Fabricate(:user, first_name: 'Albus', last_name: 'Dumbledore', email: 'admin@test.kiwi', password: 'admin', dni: '2487342387')
 
-Course.unscoped.asc(:full_name).limit(5).each {|c| c.students << admin; c.save}
-Course.unscoped.desc(:full_name).limit(5).each {|c| c.teachers << admin; c.save}
+admin = Fabricate(:user, email: 'admin@test.kiwi', password: 'admin', dni: '2487342387')
+student = Fabricate(:user, email: 'student@test.kiwi', password: 'student', dni: '3487342387')
+teacher = Fabricate(:user, email: 'teacher@test.kiwi', password: 'teacher', dni: '3587342387')
+course = Course.create(full_name: 'Diseño Web', abbreviation: 'DW')
+
+course.teachers << teacher
+course.teachers << admin
+course.students << student
+course.save
+
+
+Course.unscoped.asc(:full_name).limit(2).each {|c| c.students << admin; c.save}
+Course.unscoped.desc(:full_name).limit(2).each {|c| c.teachers << admin; c.save}
 
 20.times do |i|
   Fabricate(:user)
 end
 
 students = 10.times.collect { Fabricate(:user) }
-teachers = 10.times.collect { Fabricate(:user) }
+teachers = 3.times.collect { Fabricate(:user) }
 
 course  = Course.create(full_name: 'Course with people', abbreviation: 'AAA')
 course.students = students
-course.teachers = teachers
+course.teachers = teachers << admin
 course.save
