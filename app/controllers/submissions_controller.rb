af2603cb45dabb7cd193081960b679a7ada5a6e9
Nerian <siotopo@gmail.com>
2012-03-12 19:40:05 +0100

    Teacher should have a link to the task when viewing the submission. Students should not.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,7 +1,10 @@
 class SubmissionsController < ApplicationController
   
   def show
-    
+    @course = Course.find(params[:course_id])
+    @submission = Submission.find(params[:id])
+    @task = @submission.task
+    @user = current_user
   end
   
 end
