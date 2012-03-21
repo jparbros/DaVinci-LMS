50d5e006323e1ba04625be5911dbabb3fe991542
Nerian <siotopo@gmail.com>
2012-03-21 15:50:45 +0100

    activate the autoclose options for date-picker

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-	// $('.data-datepicker').live().datepicker({'format': 'yyyy-mm-dd', 'weekstart': 1});
 	$(document).on("focus", ".data-datepicker", function(e){
-	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1});
+	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
 	  });
 });
