8265c0b360d7783f68a26dae44f2cfbe259a486d
Nerian <siotopo@gmail.com>
2012-03-25 23:30:06 +0200

    Remove courses from student

--- app/widgets/management/users/student_in_widget.rb
+++ app/widgets/management/users/student_in_widget.rb
@@ -1,7 +1,9 @@
 class Management::Users::StudentInWidget < Apotomo::Widget
 
   responds_to_event :activate_add_mode
+  responds_to_event :activate_remove_mode
   responds_to_event :add_as_student
+  responds_to_event :remove
   responds_to_event :done
 
   helper :application
@@ -25,14 +27,33 @@ class Management::Users::StudentInWidget < Apotomo::Widget
     course = Course.find(evt[:course_id])
     course.students << user
     if course.save
-      @message = "<a href='/management/users/#{user.id}'>#{user.name}</a> is now a student in <a href='/management/courses/#{course.id}'>#{course.full_name}</a> "
+      @message = "Added <a href='/management/users/#{user.id}'>#{user.name}</a> as a student in <a href='/management/courses/#{course.id}'>#{course.full_name}</a> "
       render({:state => :activate_add_mode}, evt)
     else
       @message = "Something didn't work as expected. Reload the page and try again."
       render({:state => :activate_add_mode}, evt)
     end
   end
-
+  
+  def activate_remove_mode(evt)
+    user = User.find(evt[:user_id])
+    courses = user.student_in
+    update :view => :remove_mode, locals: {user: user, candidates: candidates(courses)}
+  end
+  
+  def remove(evt)
+    user = User.find(evt[:user_id])
+    course = Course.find(evt[:course_id])
+    course.students.delete(user)
+    if course.save
+      @message = "Removed <a href='/management/users/#{user.id}'>#{user.name}</a> from <a href='/management/courses/#{course.id}'>#{course.full_name}</a>"
+      render({:state => :activate_remove_mode}, evt)
+    else
+      @message = "Something didn't work as expected. Reload the page and try again."
+      render({:state => :activate_remove_mode}, evt)
+    end
+  end
+  
   def candidates(courses=[])
     if courses.empty?
       # return User.all.collect { |user| {'student_id' => user.id, 'value' => user.name} }
