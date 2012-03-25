5619f22284665a01c09a57decdc47191e0f51033
Nerian <siotopo@gmail.com>
2012-03-25 18:58:04 +0200

    Organise the tasks and submissions according to its state. Closes #8 #9 #10

--- app/widgets/courses/course_panel_widget.rb
+++ app/widgets/courses/course_panel_widget.rb
@@ -12,10 +12,14 @@ class Courses::CoursePanelWidget < Apotomo::Widget
   
   def tasks(course, current_user)
     if current_user.student?(course)
-      submissions = Submission.where(:user_id => current_user.id, :task_id.in => course.tasks.map(&:id))
-      render view: :tasks_student, locals: {course: course, submissions: submissions}
-    else
-      render view: :tasks_teacher, locals: {course: course, tasks: course.tasks}
+      render view: :tasks_student, locals: {course: course, 
+          active_submissions: active_submissions(current_user, course), 
+          archived_submissions: archived_submissions(current_user, course)}
+    else            
+      render view: :tasks_teacher, locals: {course: course, 
+        active_tasks: active_tasks(course), 
+        pending_revision_tasks: pending_revisions_tasks(course), 
+        archived_tasks: archived_tasks(course)}
     end
   end
   
@@ -26,5 +30,25 @@ class Courses::CoursePanelWidget < Apotomo::Widget
   def students(course)
     render view: :students, locals: {students: course.students}
   end
-
+  
+  private
+    def active_submissions(current_user, course)
+      Submission.where(:user_id => current_user.id, :task_id.in => course.tasks.map(&:id), :mark => nil)
+    end
+    
+    def archived_submissions(current_user, course)
+      Submission.where(:user_id => current_user.id, :task_id.in => course.tasks.map(&:id), :mark.ne => nil)
+    end
+    
+    def active_tasks(course)
+      course.tasks.where(:last_day_to_submit.gt => Date.today )      
+    end
+    
+    def pending_revisions_tasks(course)
+      course.tasks.where(:last_day_to_submit.lt => Date.today).select{|t| t.submissions.where(:mark.ne => nil).empty?}
+    end
+    
+    def archived_tasks(course)
+      course.tasks.where(:last_day_to_submit.lt => Date.today ).select{|t| t.submissions.where(:mark => nil).empty?}
+    end
 end
