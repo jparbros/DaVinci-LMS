3316a3ea7b08c292aeb9293d4caa6653c2fd2044
Nerian <siotopo@gmail.com>
2012-03-27 16:31:55 +0200

    typo

--- app/widgets/management/courses/students_widget.rb
+++ app/widgets/management/courses/students_widget.rb
@@ -4,20 +4,20 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
   responds_to_event :done
   responds_to_event :add_student
   responds_to_event :remove_student
-  
+
   helper :application
 
   def display(course)    
     render locals: {course: course}
   end
-  
+
   def set_remove_mode(evt)
     course = Course.find(params[:course_id])
     students = course.students
     candidates = candidates(students)
     update view: :remove_mode, locals: {course: course, students: students, candidates: candidates}
   end
-  
+
   def remove_student(evt)
     student = User.find(evt[:student_id])
     course = Course.find(evt[:course_id])
@@ -25,14 +25,14 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
     @message = "Removed <a href='/management/users/#{student.id}'>#{student.name}</a> from <a href='/management/courses/#{course.id}'>#{course.full_name}</a>"
     render({state: :set_remove_mode}, course)
   end
-  
+
   def set_add_mode(evt)
     course = Course.find(evt[:course_id])
     students = course.students
     candidates = candidates(User.not_in(_id: course.student_ids))
     update view: :add_mode, locals: {course: course, students: students, candidates: candidates}
   end
-  
+
   def add_student(evt)
     student = User.find(evt[:student_id])
     course = Course.find(evt[:course_id])
@@ -42,11 +42,11 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
       render({state: :set_add_mode}, course)
     end
   end
-  
+
   def done(evt)
     update({state: :display}, Course.find(evt[:course_id]))
   end
-  
+
   def private
     def candidates(users=[])
       if users.empty?
@@ -55,4 +55,5 @@ class Management::Courses::StudentsWidget < Apotomo::Widget
         return users.collect { |user| {'student_id' => user.id, 'value' => user.name} }
       end
     end
+  end
 end
