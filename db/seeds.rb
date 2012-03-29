13a1d24e90a8628f447f100abe14adb27611abd3
Nerian <siotopo@gmail.com>
2012-03-29 16:41:52 +0200

    Use english names for courses in the seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,42 +1,23 @@
 # encoding: utf-8
 
-Course.create(full_name: 'Metodología y tecnología de la programación I', abbreviation: 'MTP')
-Course.create(full_name: 'Fundamentos matemáticos de la informática I', abbreviation: 'FMI1')
-Course.create(full_name: 'Fundamentos de sistemas de información', abbreviation: 'FSI')
-Course.create(full_name: 'Técnicas de organización y gestión empresarial I', abbreviation: 'TOG1')
-Course.create(full_name: 'Estructura y tecnología de computadores', abbreviation: 'ETC')
-Course.create(full_name: 'Estructura de datos y de la información', abbreviation: 'EDI')
-
-Course.create(full_name: 'Sistemas integrados de gestión I', abbreviation: 'SIG1')
-Course.create(full_name: 'Bases de datos', abbreviation: 'BD')
-Course.create(full_name: 'Estadística', abbreviation: 'EST')
-Course.create(full_name: 'Técnicas de organización y gestión empresarial II', abbreviation: 'TOG2')
-Course.create(full_name: 'Fundamentos matemáticos de la informática II', abbreviation: 'FMI2')
-Course.create(full_name: 'Metodología y tecnología de la programación II', abbreviation: 'MTP2')
-Course.create(full_name: 'Sistemas operativos', abbreviation: 'SO')
-Course.create(full_name: 'Ingeniería del software de gestión I', abbreviation: 'ISG1')
-
-Course.create(full_name: 'Sistemas Hipermedia', abbreviation: 'SH')
-Course.create(full_name: 'Redes Telemáticas', abbreviation: 'RT')
-Course.create(full_name: 'Sistemas integrados de gestión II', abbreviation: 'SIG2')
-Course.create(full_name: 'Seguridad Informática', abbreviation: 'SI')
-Course.create(full_name: 'Ingeniería del software de gestión II', abbreviation: 'ISG2')
-Course.create(full_name: 'Sistemas de información global', abbreviation: 'SIGL')
-Course.create(full_name: 'Seguridad y medios de pagos', abbreviation: 'SYP')
-Course.create(full_name: 'Programación Web Avanzada', abbreviation: 'PWA')
-Course.create(full_name: 'Ampliación de bases de datos', abbreviation: 'BD2')
-
+Course.create(full_name: 'Introduction to Databases', abbreviation: 'CS-145')
+Course.create(full_name: 'Design and Analysis of Algorithms', abbreviation: 'CS-161')
+Course.create(full_name: 'Introduction to Computer Vision', abbreviation: 'CS-223B-')
+Course.create(full_name: 'Advanced Topics in Networking', abbreviation: 'CS-244')
+Course.create(full_name: 'Introduction to Computer Networks', abbreviation: 'CS-244-A')
+Course.create(full_name: 'Parallel Computer Architecture and Programming', abbreviation: 'CS-315A')
+Course.create(full_name: 'Projects in Computer Networks', abbreviation: 'CS-344')
+Course.create(full_name: 'Computer Graphics Geometric Modeling', abbreviation: 'CS-348-A')
 
 admin = Fabricate(:user, email: 'admin@test.kiwi', password: 'admin', dni: '2487342387')
 student = Fabricate(:user, email: 'student@test.kiwi', password: 'student', dni: '3487342387')
 teacher = Fabricate(:user, email: 'teacher@test.kiwi', password: 'teacher', dni: '3587342387')
-course = Course.create(full_name: 'Diseño Web', abbreviation: 'DW')
+course = Course.create(full_name: 'Web Development', abbreviation: 'CS-124')
 
 course.teachers << teacher
 course.teachers << admin
 course.students << student
 course.save
-
 
 Course.unscoped.asc(:full_name).limit(2).each {|c| c.students << admin; c.save}
 Course.unscoped.desc(:full_name).limit(2).each {|c| c.teachers << admin; c.save}
@@ -48,7 +29,7 @@ end
 students = 10.times.collect { Fabricate(:user) }
 teachers = 3.times.collect { Fabricate(:user) }
 
-course  = Course.create(full_name: 'Course with people', abbreviation: 'AAA')
+course  = Course.create(full_name: 'Course with a lot of people', abbreviation: 'AAA')
 course.students = students
 course.teachers = teachers << admin
 course.save
