da0883d3f1625dae27abd444cd9d07da87a89ace
Nerian <siotopo@gmail.com>
2012-04-23 17:13:55 +0200

    Hide the nav menu when a option is selected

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
 	$(document).on("focus", ".data-datepicker", function(e){
 	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
 	  });
+	  
+  $('[data-pjax-container]')
+    .on('pjax:start', function() { $('.nav-collapse').collapse('hide') })
 });
