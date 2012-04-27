b8ffa513796f9ff6f8ee1c602c64d37f1e3ba34d
Nerian <siotopo@gmail.com>
2012-04-27 19:15:33 +0200

    localisation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,10 +3,8 @@ $(document).ready(function() {
 	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
 	});
 	  	
-	$('a[rel=popover]').popover({placement: 'bottom'});
-	$('[data-pjax-container]')
-    .on('pjax:end', function() { $('a[rel=popover]').popover({placement: 'bottom'}); });
-    
+	$('a[rel=popover]').popover({placement: 'bottom'});    
+  $(document).on("ajaxComplete", function() { $('a[rel=popover]').popover({placement: 'bottom'}); });
 		  
   $('[data-pjax-container]')
     .on('pjax:start', function() { $('.nav-collapse').collapse('hide') });
