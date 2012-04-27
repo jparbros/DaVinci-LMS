3810ffbe6a623d6ab0800704916eb0b8dfa4df91
Nerian <siotopo@gmail.com>
2012-04-27 18:03:58 +0200

    Use an event handle

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,15 +1,13 @@
 $(document).ready(function() {
 	$(document).on("focus", ".data-datepicker", function(e){
 	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
-	  });
+	});
 	  
-	$('[data-popover]').popover();
-	$("a[rel=popover]")
-        .popover({placement: 'bottom'})
-        .click(function(e) {
-          e.preventDefault()
-        });
-	  
+	$(document).on("mouseover", "a[rel=popover]", function(e){
+	    $(this).popover({placement: 'bottom'});
+	    $(this).popover('show');
+	});
+		  
   $('[data-pjax-container]')
     .on('pjax:start', function() { $('.nav-collapse').collapse('hide') })
 });
