c9134f8df72842ff66a34e9aa6792219bd2044db
Nerian <siotopo@gmail.com>
2012-04-27 17:57:11 +0200

    Add info links

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
 	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
 	  });
 	  
+	$('[data-popover]').popover();
+	$("a[rel=popover]")
+        .popover({placement: 'bottom'})
+        .click(function(e) {
+          e.preventDefault()
+        });
+	  
   $('[data-pjax-container]')
     .on('pjax:start', function() { $('.nav-collapse').collapse('hide') })
 });
