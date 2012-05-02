103336e5620c79002e6411c2e1be0720686b0d9b
Nerian <siotopo@gmail.com>
2012-05-02 15:09:08 +0200

    delete features: submissions and tasks

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
 	$(document).on("focus", ".data-datepicker", function(e){
-	    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
+    $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
 	});
 		  
   $('[data-pjax-container]')
     .on('pjax:start', function() { $('.nav-collapse').collapse('hide') });
     
-  $(document).on("ajaxComplete ready", function(){
-    $("[data-behaviour~='toggle']").on('click', function() { 
+  $(document).on('click', "[data-behaviour~='toggle']", function() { 
       $($(this).data('target')).toggle();
       alternative_text = $(this).data('alternative-text');
       $(this).data('alternative-text', $(this).html());
       $(this).html(alternative_text);
     });
-    
-    $("[data-behaviour~='toggle-text-length']").on('click', function() { 
+  
+  $(document).on('click', "[data-behaviour~='toggle-text-length']", function() { 
       $(this).parent('li').hide(); 
-      $($(this).data('target')).show(); });
-          
-    $('a[rel=popover]').popover({placement: 'bottom'});        
+      $($(this).data('target')).show(); 
+    });
+    
+  $(document).on('hover', '.title', function(){
+    $('a[rel=popover]').popover({placement: 'bottom'});
   });
+  
 });
--- app/widgets/tasks/submissions_widget.rb
+++
@@ -1,9 +0,0 @@
-class Tasks::SubmissionsWidget < Apotomo::Widget
-
-  def display(course, task)
-    graded_submissions = task.submissions.where(:mark.ne => nil).sort_by {|s| s.user.name}
-    ungraded_submissions = task.submissions.where(:mark => nil).sort_by {|s| s.user.name}
-    render locals: {course: course, graded_submissions: graded_submissions, ungraded_submissions: ungraded_submissions}
-  end
-
-end
