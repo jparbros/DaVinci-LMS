c2fb9e7d90c1f695fc7a9a12179d6d0768bc0d74
Nerian <siotopo@gmail.com>
2012-05-03 13:57:14 +0200

    remove old code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Davinci::Application.routes.draw do
-    
+
   scope "(:locale)", :locale => /en|es/ do
     namespace :management do
       resources :courses, :only => [:index, :new, :show]
@@ -9,17 +9,15 @@ Davinci::Application.routes.draw do
 
     resources :courses, :only => [:show] do
       match "file" => "courses#add_file"
-      resources :tasks, :only => [:new, :show]    
-      resources :submissions, :only => [:show] { match "add_file" => "submissions#add_file" }    
     end
 
     resources :users, only: [:show]
 
     resources :session, only: [:create]
     match 'login' => 'session#new', :as => :login
-    match 'logout' => 'session#destroy', :as => :logout    
+    match 'logout' => 'session#destroy', :as => :logout
   end
-  
+
   match "uploads/:id/*name" => "gridfs#serve", :as => :serve
 
   match '/:locale' => 'home#index'
